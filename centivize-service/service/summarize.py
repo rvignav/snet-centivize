163fec5432dca8604124dc5399f1587f4eab0046
rvignav <rvignav@gmail.com>
2021-01-11 15:03:59 -0800

    Update summary

--- centivize-service/service/summarize.py
+++ centivize-service/service/summarize.py
@@ -6,11 +6,17 @@ from transformers import pipeline
 def setup():
     nltk.download('punkt')
 
-def summarize(par, num=11):
+def summarize(par, percent_len=0.5):
     qa = pipeline('summarization')
-    sent_num = num
     graf = par
     graf = tokenize.sent_tokenize(graf)
+
+    sent_num = 3 // percent_len
+    if sent_num <= 3 or len(graf) <= 3:
+        return "Parameter combination results in an undesirable output. Consider either lengthening the input text or changing the percent_len argument."
+    if sent_num >= len(graf):
+        return "percent_len is below the minimum value for this particular sequence, lowering it further will not shorten the output"
+    
     final = []
     for i in range(len(graf)):
         if i % sent_num == 0:
